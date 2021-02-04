0fccf269d04fe2348792708a9157914c812c0a6a
j-onas <jonas@hjulle.com>
2021-02-04 20:20:09 +0100

    Images, cards, css

--- js/api/main.js
+++ js/api/main.js
@@ -1,25 +1,61 @@
-const clientID = "ZCXBO3HBIWCGYD0U1203RBDRXTPHKXBBPCYV4TRUHEKIDQIR"
-const clientSecret = "1X4SB3KHTPVBK1VG3KEJJTNARF5CJTZUVAYVKGQWOVEL1L3I"
-const versionDate = dateToString();
+'use strict';
 
 window.onload=function(){
-    var inputBox = document.querySelector("input");
-    let timeOut = null;
+    let inputBox = document.querySelector("input");
+    let loading = document.getElementById("loading");
 
-    inputBox.addEventListener("keyup", function(){
-        clearTimeout(timeOut);
-
-        timeOut = setTimeout(function() {
-            fetch("https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id="+clientID+"&client_secret="+clientSecret+"&v="+versionDate+"")
-                                                                                                    .then(resp => resp.json())
-                                                                                                    .then(res => console.log(res));
-            console.log(inputBox.value)
-
-        }, 1000);
-        ;
+    inputBox.addEventListener("keypress", function(e){
+        if(e.key === "Enter"){
+            clearCards();
+            fetchApi(inputBox.value);
+        }
     });
 };
 
+function fetchApi(userInput){
+    console.log("User input: "+userInput)
+
+    const clientID = "ZCXBO3HBIWCGYD0U1203RBDRXTPHKXBBPCYV4TRUHEKIDQIR"
+    const clientSecret = "1X4SB3KHTPVBK1VG3KEJJTNARF5CJTZUVAYVKGQWOVEL1L3I"
+    let versionDate = dateToString();
+    let cardParent = document.getElementById("cards");
+    fetch("https://api.foursquare.com/v2/venues/search?near="+userInput+"&client_id="+clientID+"&client_secret="+clientSecret+"&v="+versionDate+"").then(resp => resp.json())
+        .then(res => {
+             Object.entries(res).forEach(entry => {
+                const [key, value] = entry;
+                if(key == "response"){
+                    value["venues"].forEach(v => {
+                        let lat = v["location"]["lat"]
+                        let lng = v["location"]["lng"]
+                        let name = v["name"].toString();
+                        let address = "https://www.google.se/maps/@"+lat+","+lng+",20z"
+                        let card = document.createElement("div");
+                        let cardName = document.createElement("h3");
+                        let cardImage = document.createElement("img")
+                        let cardAddress = document.createElement("div")
+                        let cardAddressURL = document.createElement("a")
+                        let cardUnorderedList = document.createElement("ul")
+                        
+
+                        card.setAttribute("id", "card");
+                        cardName.innerHTML = name;
+                        cardImage.setAttribute("src", "img/placeholder.jpg")
+                        cardImage.setAttribute("alt", "")
+                        cardAddress.setAttribute("align", "center")
+                        cardAddressURL.setAttribute("href", address)
+                        cardAddressURL.setAttribute("target", "_blank")
+                        cardAddressURL.innerHTML = '<p>Address</p>'
+                        cardUnorderedList.innerHTML = ""
+                        card.append(cardName)
+                        card.append(cardImage)
+                        card.append(cardAddress)
+                        cardAddress.append(cardAddressURL)
+                        cardParent.appendChild(card)
+                    });
+                };
+            });            
+        }).catch(error => console.log(error));                                                                
+    }
 
 function dateToString(){
     let today = new Date();
@@ -36,4 +72,16 @@ function dateToString(){
     return dateYear.toString()+dateMonth.toString()+dateDay.toString();
 }
 
+function clearCards() {
 
+    //Hämta div-taggen som heter cards och ge den variablen cardParent
+    var cardParent = document.getElementById("cards");
+
+    //Hitta alla taggar under "cards"
+    //Loopa igenom alla taggar som finns och ta bort dom
+    var cardChildren = cardParent.childNodes;
+    for(var i = cardChildren.length-1; i >= 0; i--){
+        var cardChild = cardChildren[i];
+        cardChild.parentNode.removeChild(cardChild);
+    }
+}
